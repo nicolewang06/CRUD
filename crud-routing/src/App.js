8e71978cd69846deea0f65f84d163d8242ae96b0
Nicole Wang <nicolewang06@gmail.com>
2021-01-08 16:05:22 -0500

    edit functionality

--- crud-routing/src/App.js
+++ crud-routing/src/App.js
@@ -12,9 +12,9 @@ import {
 } from "react-router-dom";
 import './App.css'
 
-const WINES_URL = 'http://myapi-profstream.herokuapp.com/api/06f82d/wines'
-const BOOKS_URL = 'http://myapi-profstream.herokuapp.com/api/ffbede/books'
-const PEOPLE_URL = 'http://myapi-profstream.herokuapp.com/api/fdf5d8/persons'
+const WINES_URL = 'http://myapi-profstream.herokuapp.com/api/779738/wines/'
+const BOOKS_URL = 'http://myapi-profstream.herokuapp.com/api/ffbede/books/'
+const PEOPLE_URL = 'http://myapi-profstream.herokuapp.com/api/fdf5d8/persons/'
 
 class App extends React.Component {
   constructor(props) {
@@ -76,6 +76,9 @@ class Wines extends React.Component {
     this.state = {}
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.selectWine = this.selectWine.bind(this);
+    this.editWine = this.editWine.bind(this);
+    this.submitEditedWine = this.submitEditedWine.bind(this);
   }
 
   async getWines() {
@@ -127,13 +130,40 @@ class Wines extends React.Component {
   async handleDelete(id) {
     console.log(WINES_URL + id);
     try{
-      const res = await axios.delete(WINES_URL + '/' + id); // target wine id
+      const res = await axios.delete(WINES_URL + id); // target wine id
       console.log(res.data);
 
       const updateRes = await axios.get(WINES_URL);
       this.setState( { wines: updateRes.data } )
     } catch(e) {
       console.error(e.message)
+    }
+  }
+
+  selectWine(selectedWine) {
+    this.setState ( { selectedWine } );
+    //console.log(wine);
+  }
+
+  editWine(e) {
+    const { name, value } = e.target;
+    this.setState( { ...this.state, selectedWine: { ...this.state.selectedWine, [name]: value } } );
+    //console.log(e.target.value);
+  }
+
+  async submitEditedWine(e) {
+    e.preventDefault();
+    try {
+      const editedWine = this.state.selectedWine; // this obj has an id
+      console.log(editedWine)
+      // to send our patch to url + /id
+      const focusWine = WINES_URL + editedWine.id
+      const res = await axios.patch(focusWine, editedWine);
+
+      const resRefresh = await axios.get(WINES_URL);
+      this.setState( { wines: resRefresh.data } );
+    } catch(e) {
+      console.error(e);
     }
   }
 
@@ -144,51 +174,97 @@ class Wines extends React.Component {
           {/* render info */}
           {
             this.state.wines && this.state.wines.map(wine => (
-              <li id={ wine.id }>
+              <li key={ wine.id }>
                 { wine.name }: price { wine.price }
                 <button onClick={ () => this.handleDelete(wine.id) }>Delete wine</button>
+                <button onClick={ () => this.selectWine(wine) }>Edit Wine</button>
               </li>
               ))
           }
         </ul>
         <form className="new-wine-form"
           onChange={ this.handleChange }
-          onSubmit={ this.handleSubmit }
-          >
+          onSubmit={ this.handleSubmit }>
           <label>
             Wine Name:
             <input type="text" name="name" />
-          </label><br></br>
+          </label>
           <label>
             Year wine was made:
             <input type="text" name="year" />
-          </label><br></br>
+          </label>
           <label>
             Grapes used:
             <input type="text" name="grapes" />
-          </label><br></br>
+          </label>
           <label>
             Country of wine:
             <input type="text" name="country" />
-          </label><br></br>
+          </label>
           <label>
             Wine Region:
             <input type="text" name="region" />
-          </label><br></br>
+          </label>
           <label>
             Description of wine:
             <input type="text" name="description" />
-          </label><br></br>
+          </label>
           <label>
             Picture url:
             <input type="text" name="picture" />
-          </label><br></br>
+          </label>
           <label>
             Price:
             <input type="text" name="price" />
-          </label><br></br>
+          </label>
           <input type="submit" />
         </form>
+
+        <hr></hr>
+            { /* we want to show the form */ }
+            { /* only after we select a wine */ }
+            { /* if this.state.selectedWine exists */ }
+            { /* render this form below */ }
+            { /* this.state.selectedWine && formBelow */ }
+        { 
+        this.state.selectedWine && <form className="wine-edit-form"
+          onChange={ this.editWine}
+            onSubmit = {this.submitEditedWine}>
+          <label>
+            Wine Name:
+            <input type="text" name="name" defaultValue={ this.state.selectedWine.name } />
+          </label>
+          <label>
+            Year wine was made:
+            <input type="text" name="year" defaultValue={ this.state.selectedWine.year }/>
+          </label>
+          <label>
+            Grapes used:
+            <input type="text" name="grapes" defaultValue={ this.state.selectedWine.grapes }/>
+          </label>
+          <label>
+            Country of wine:
+            <input type="text" name="country" defaultValue={ this.state.selectedWine.country }/>
+          </label>
+          <label>
+            Wine Region:
+            <input type="text" name="region" defaultValue={ this.state.selectedWine.region }/>
+          </label>
+          <label>
+            Description of wine:
+            <input type="text" name="description" defaultValue={ this.state.selectedWine.description }/>
+          </label>
+          <label>
+            Picture url:
+            <input type="text" name="picture" defaultValue={ this.state.selectedWine.picture }/>
+          </label>
+          <label>
+            Price:
+            <input type="text" name="price" defaultValue={ this.state.selectedWine.price }/>
+          </label>
+          <input type="submit" />
+        </form>
+  }
     </div>
     )
   }
@@ -201,12 +277,15 @@ class Books extends App {
     this.state = {}
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.selectBook = this.selectBook.bind(this);
+    this.editBook = this.editBook.bind(this);
+    this.submitEditedBook = this.submitEditedBook.bind(this);
   }
 
   async getBooks() {
     try{
       const res = await axios.get(BOOKS_URL);
-      console.log(res.data)
+      //console.log(res.data)
       this.setState( { books: res.data } )
     } catch(er) {
       console.error(er.message);
@@ -239,13 +318,38 @@ class Books extends App {
   async handleDelete(id) {
     console.log(BOOKS_URL + id);
     try{
-      const res = await axios.delete(BOOKS_URL + '/' + id);
+      const res = await axios.delete(BOOKS_URL + id);
       console.log(res.data);
 
       const updateRes = await axios.get(BOOKS_URL);
       this.setState( { books: updateRes.data } )
     } catch(er) {
       console.error(er.message)
+    }
+  }
+
+  selectBook(selectedBook) {
+    this.setState( { selectedBook } );
+  }
+
+  editBook(e) {
+    const { name, value } = e.target;
+    this.setState( { ...this.state, selectedBook: { ...this.state.selectedBook, [name]: value } } );
+  }
+
+  async submitEditedBook(e) {
+    e.preventDefault();
+    try {
+      const editedBook = this.state.selectedBook;
+      console.log(editedBook)
+
+      const focusBook = BOOKS_URL + editedBook.id
+      const res = await axios.patch(focusBook, editedBook);
+
+      const resRefresh = await axios.get(BOOKS_URL);
+      this.setState( { books: resRefresh.data } );
+    } catch(e) {
+      console.error(e);
     }
   }
 
@@ -256,7 +360,9 @@ class Books extends App {
           {
             this.state.books && this.state.books.map(book => (
               <li key={ book.id }>
-                {book.title}, Author: {book.author} <button onClick={ () => this.handleDelete(book.id) }>Delete book</button>
+                {book.title}, Author: {book.author}
+                <button onClick={ () => this.handleDelete(book.id) }>Delete book</button>
+                <button onClick={ () => this.selectBook(book) }>Edit Book</button>
               </li>
             ))
           }
@@ -267,21 +373,46 @@ class Books extends App {
           <label>
             Title:
             <input type="text" name="title" />
-          </label><br></br>
+          </label>
           <label>
             Author:
             <input type="text" name="author" />
-          </label><br></br>
+          </label>
           <label>
             Release Date:
             <input type="text" name="release_date" />
-          </label><br></br>
+          </label>
           <label>
             Image:
             <input type="text" name="image" />
-          </label><br></br>
+          </label>
           <input type="submit" />
         </form>
+
+        <hr></hr>
+        {
+        this.state.selectedBook && <form className="book-edit-form"
+        onChange = { this.editBook }
+        onSubmit = { this.submitEditedBook }>
+          <label>
+            Title:
+            <input type="text" name="title" defaultValue={ this.state.selectedBook.title }/>
+          </label>
+          <label>
+            Author:
+            <input type="text" name="author" defaultValue={ this.state.selectedBook.author }/>
+          </label>
+          <label>
+            Release Date:
+            <input type="text" name="release_date" defaultValue={ this.state.selectedBook.release_date } />
+          </label>
+          <label>
+            Image:
+            <input type="text" name="image" defaultValue={ this.state.selectedBook.image }/>
+          </label>
+          <input type="submit" />
+        </form>
+  }
       </div>
     )
   }
@@ -293,6 +424,9 @@ class People extends App {
     this.state = {};
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.selectPerson = this.selectPerson.bind(this);
+    this.editPerson = this.editPerson.bind(this);
+    this.submitEditedPerson = this.submitEditedPerson.bind(this);
   }
 
   async getPeople() {
@@ -327,7 +461,7 @@ class People extends App {
 
   async handleDelete(id) {
     try {
-      const res = await axios.delete(PEOPLE_URL + '/' + id);
+      const res = await axios.delete(PEOPLE_URL + id);
 
       const updateRes = await axios.get(PEOPLE_URL);
       this.setState({ people: updateRes.data });
@@ -340,14 +474,41 @@ class People extends App {
     this.getPeople();
   }
 
+  selectPerson(selectedPerson) {
+    this.setState( { selectedPerson } );
+  }
+
+  editPerson(e) {
+    const { name, value } = e.target;
+    this.setState( { ...this.state, selectedPerson: { ...this.state.selectedPerson, [name]: value } } );
+  }
+
+  async submitEditedPerson(e) {
+    e.preventDefault();
+    try{
+      const editedPerson = this.state.selectedPerson;
+      console.log(editedPerson)
+
+      const focusPerson = PEOPLE_URL + editedPerson.id
+      const res = await axios.patch(focusPerson, editedPerson);
+
+      const resRefresh = await axios.get(PEOPLE_URL);
+      this.setState( { people: resRefresh.data } );
+    } catch(e) {
+      console.error(e);
+    }
+  }
+
   render () {
     return(
       <div className="people">
         <ul>
           {
             this.state.people && this.state.people.map(person => (
-              <li id={person.id}>
-                { person.firstname} { person.lastname } <button onClick= { () => this.handleDelete(person.id) }>Delete Person</button>
+              <li key={person.id}>
+                { person.firstname} { person.lastname }
+                <button onClick= { () => this.handleDelete(person.id) }>Delete Person</button>
+                <button onClick= { () => this.selectPerson(person) }>Edit Person</button>
               </li>
             ))
           }
@@ -358,21 +519,46 @@ class People extends App {
           <label>
             First Name: 
             <input type="text" name="firstname" />
-          </label><br></br>
+          </label>
           <label>
             Last Name: 
             <input type="text" name="lastname" />
-          </label><br></br>
+          </label>
           <label>
             Email Address: 
             <input type="text" name="email" />
-          </label><br></br>
+          </label>
           <label>
             Username: 
             <input type="text" name="username" />
-          </label><br></br>
+          </label>
           <input type="submit" />
         </form>
+
+        <hr></hr>
+{
+        this.state.selectedPerson && <form className="people-edit-form"
+          onChange = { this.editPerson }
+          onSubmit = { this.submitEditedPerson }>
+          <label>
+            First Name: 
+            <input type="text" name="firstname" defaultValue={ this.state.selectedPerson.firstname }/>
+          </label>
+          <label>
+            Last Name: 
+            <input type="text" name="lastname" defaultValue={ this.state.selectedPerson.lastname }/>
+          </label>
+          <label>
+            Email Address: 
+            <input type="text" name="email" defaultValue={ this.state.selectedPerson.email }/>
+          </label>
+          <label>
+            Username: 
+            <input type="text" name="username" defaultValue={ this.state.selectedPerson.username } />
+          </label>
+          <input type="submit" />
+        </form>
+}
       </div>
     )
   }
